0fc7985f2b8a35b64a3d0226e9a0855dab65eafd
NightCrystal169 <35397935+NightCrystal169@users.noreply.github.com>
2018-01-14 14:56:04 +0900

    Update run.js

--- run.js
+++ run.js
@@ -15,7 +15,7 @@ Client.on("message", message => {
   command = command.slice(prefix.length);
   let args = message.content.split(" ").slice(1);
   if (command === "help") {
-    const embed = new Discord.RichEmbed().setColor(0x0000FF).setAuthor("HELP", "https://goo.gl/ENFFtn").addField("-help: ", "コマンドリストを表示します。").addField("-ping: ", "通信速度を表示します。").addField("-lobi-allkick", "900人以上居るCorePvPのメンバーを全員Kickします。(機能実装予定)")
+    const embed = new Discord.RichEmbed().setColor(0x0000FF).setAuthor("HELP", "https://goo.gl/ENFFtn").addField("-help: ", "コマンドリストを表示します。").addField("-ping: ", "通信速度を表示します。")
     message.channel.send(embed);
   } else
   if (command === "ping") {
