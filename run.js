ab072d05aa5f40970d15683107a0700abd3f9ced
NightCrystal169 <35397935+NightCrystal169@users.noreply.github.com>
2018-01-19 22:51:33 +0900

    見やすく

--- run.js
+++ run.js
@@ -16,7 +16,11 @@ Client.on("message", message => {
   command = command.slice(prefix.length);
   let args = message.content.split(" ").slice(1);
   if (command === "help") {
-    const embed = new Discord.RichEmbed().setColor(0x0000FF).setAuthor("HELP", "https://goo.gl/ENFFtn").addField("-help: ", "コマンドリストを表示します。").addField("-ping: ", "通信速度を表示します。")
+    const embed = new Discord.RichEmbed()
+    .setColor(0x0000FF)
+    .setAuthor("HELP", "https://goo.gl/ENFFtn")
+    .addField("-help: ", "コマンドリストを表示します。")
+    .addField("-ping: ", "通信速度を表示します。")
     message.channel.send(embed);
   } else
   if (command === "ping") {
